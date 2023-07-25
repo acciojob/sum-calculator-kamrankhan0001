ab6f5e8941fe4c7afdf7c3c412d84ce74557384b
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-26 00:11:33 +0530

    submitting code-Wed Jul 26 2023 00:11:30 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,31 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [numbers, setNumbers] = useState([]);
+  const [sum, setSum] = useState(0);
+
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    setNumbers((prevNumbers) => [...prevNumbers, Number(inputValue)]);
+    updateSum();
+  };
+
+  const updateSum = () => {
+    // Use setTimeout to update the sum asynchronously
+    setTimeout(() => {
+      const newSum = numbers.reduce((acc, num) => acc + num, 0);
+      setSum(newSum);
+    }, 0);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Sum Calculator</h2>
+      <input type="number" onChange={handleInputChange} />
+      <div>
+        <strong>Sum:</strong> {sum}
+      </div>
     </div>
   )
 }
